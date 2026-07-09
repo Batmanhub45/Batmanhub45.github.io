dc0e9c1d0a67bfc0950e71961df0b90345f7ce12
Batmanhub45 <amwdunn@icloud.com>
2026-07-09 14:21:46 -0400

    Fix coordinates and marker definitions in travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -1,5 +1,5 @@
-// 42.7466° N latitude and -75.7700 W longitude
-var coordinates = [42.7466, -75.7700];
+// 40.7128° N latitude and 74.0060° W
+var coordinates = [40.7128, 74.0060];
 var palce1 = [40.7128, 74.0060];
 var palce2 = [40.6501, -73.9496];
 var palce3 = [40.7136, -73.8281];
@@ -9,6 +9,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker(coordinates).bindPopup("<p>Hello, from New York City</p>").addTo(map);
-var marker1 = L.circle(place1,{colo: "Black", fillColor: "#0ca607", radius: 100}).bindPopup("<p>Hello, from manhattan"addTo(map);
-var marker1 = L.circle(place2,{colo: "Black", fillColor: "#0ca607", radius: 100}).bindPopup("<p>Hello, from brooklyn"addTo(map);
-var marker1 = L.circle(place3,{colo: "Black", fillColor: "#0ca607", radius: 100}).bindPopup("<p>Hello, from queens"addTo(map);
+var marker1 = L.circle(place1,{color: "Black", fillColor: "#0ca607", radius: 100}).bindPopup("<p>Hello, from manhattan").addTo(map);
+var marker1 = L.circle(place2,{color: "Black", fillColor: "#0ca607", radius: 100}).bindPopup("<p>Hello, from brooklyn").addTo(map);
+var marker1 = L.circle(place3,{color: "Black", fillColor: "#0ca607", radius: 100}).bindPopup("<p>Hello, from queens").addTo(map);
